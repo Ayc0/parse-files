5e6548b49ae9361eafdf8b3779ad1a6fc7f7ccab
Ayc0 <ayc0.benj@gmail.com>
2021-05-29 19:08:32 +0200

    change timeout in tests

--- tests/worker.js
+++ tests/worker.js
@@ -3,11 +3,11 @@ const { workerData, parentPort } = require("worker_threads");
 
 const { filePath } = workerData;
 
-// const now = Date.now();
-// while (Date.now() - now < 5000) {}
+// Add random timeout to make sure that it supports async operations and un-ordered callbacks
+const timeout = Math.random() * 1000;
 
 setTimeout(() => {
 	parentPort.postMessage(
 		fs.readFileSync(filePath, { encoding: "utf-8" }).trim().length
 	);
-}, Math.random());
+}, timeout);
