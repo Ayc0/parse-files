4af24029b1538592487c1b41a1f4e8d724c74f7f
Ayc0 <ayc0.benj@gmail.com>
2021-05-30 18:40:47 +0200

    add test for failure

--- tests/index.js
+++ tests/index.js
@@ -2,14 +2,34 @@ const path = require("path");
 const parseFiles = require("../");
 const assert = require("assert").strict;
 
-parseFiles({
-	filePattern: path.join(__dirname, "files/**"),
-	threads: 3,
-	workerPath: require.resolve("./worker.js"),
-})
-	.then((res) => {
-		console.log(res);
-		const total = res.reduce((tot, cur) => tot + cur, 0);
-		assert.equal(total, 9);
+main();
+
+function main() {
+	checkItWorks();
+	checkItThrowsOnError();
+}
+
+function checkItWorks() {
+	parseFiles({
+		filePattern: path.join(__dirname, "files/**"),
+		threads: 3,
+		workerPath: require.resolve("./worker-fine.js"),
 	})
-	.catch((error) => assert.equal(error, "no error"));
+		.then((res) => {
+			console.log(res);
+			const total = res.reduce((tot, cur) => tot + cur, 0);
+			assert.equal(total, 9);
+		})
+		.catch((error) => assert.equal(error, "no error"));
+}
+
+function checkItThrowsOnError() {
+	assert.rejects(
+		parseFiles({
+			filePattern: path.join(__dirname, "files/**"),
+			threads: 3,
+			workerPath: require.resolve("./worker-throws.js"),
+		}),
+		{ message: "error from main throw" }
+	);
+}
